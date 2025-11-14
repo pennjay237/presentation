5585989839f13c1ed9a1c91d75ca364bab5fedb1
pennjay237 <pennjude31@gmail.com>
2025-11-14 11:36:27 +0100

    adding api

--- script/index.js
+++ script/index.js
@@ -33,3 +33,23 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            const apiKey = "11845b16191d4e970987e59662f49847";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
